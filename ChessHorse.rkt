36ec78d08be3c774490f53bb5beb0181048100ea
Jonathan G Araya <jonathana1196@gmail.com>
2019-05-04 18:18:21 -0600

    Initial Commit
    Se agrega el archivo sobre el que se desarrolla el codigo.

---
+++ ChessHorse.rkt
@@ -0,0 +1,18 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-advanced-reader.ss" "lang")((modname ChessHorse) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #t #t none #f () #f)))
+;; Tarea Progamada Problema del Caballo
+;; Prof: Marco Vinicio
+;; Alumnos: Dario Rodriguez
+;;          Jonathan Guzman
+;;          Kenneth Hernandez
+
+
+;;##########################################################################Definición de variables para GUI#########################################################################
+
+
+;;###################################################################################################################################################################################
+
+;;##################################################################################### Tablero #####################################################################################
+
+;;###################################################################################################################################################################################
